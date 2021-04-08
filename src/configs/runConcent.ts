9e18fef8caf1e2e5e3ca5356deab7190273e2c55
fancyzhong <fancyzhong@tencent.com>
2021-04-08 18:22:12 +0800

    feature: add reduxDevToolPlugin

--- src/configs/runConcent.ts
+++ src/configs/runConcent.ts
@@ -1,12 +1,20 @@
 import { run } from 'concent';
-import models from '../models';
+import reduxDevToolPlugin from 'concent-plugin-redux-devtool';
 import * as msgService from 'services/message';
+import * as commonUtil from 'utils/common';
+import models from '../models';
+
+const plugins = [];
+if (commonUtil.isLocalMode()) {
+  plugins.push(reduxDevToolPlugin);
+}
 
 // load all models by concent.run api
 run(models, {
   errorHandler: (err) => {
     msgService.error(err.message);
   },
+  plugins,
   alwaysRenderCaller: true,
   isStrict: true,
 });
