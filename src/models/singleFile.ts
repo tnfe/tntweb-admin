095825048b20ff2f556939173961fc2798b64c24
fancyzhong <fancyzhong@tencent.com>
2021-07-13 09:09:39 +0800

    chore: add single model file

--- src/models/singleFile.ts
+++ src/models/singleFile.ts
@@ -1,17 +1,27 @@
 import { defineModule } from 'concent';
+import { delay } from 'utils/timer';
 
 interface St {
   value: number;
+  loading: boolean;
 }
 
 const m = defineModule({
   state: (): St => ({
     value: 1,
+    loading: false,
   }),
   reducer: {
-    add(payload: any, moduleState) {
-      return { value: moduleState.value + 1 };
-    }
+    add(payload: number | void, moduleState) {
+      const toAdd = payload === undefined ? 1 : 0;
+      return { value: moduleState.value + toAdd };
+    },
+    async asyncAdd(payload: number | void, moduleState, ac) {
+      await ac.setState({ loading: true });
+      await delay(2000);
+      await ac.dispatch(m.r.add, 100);
+      return { loading: false };
+    },
   },
   computed: {
     double(newState) {
