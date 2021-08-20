b6cc0df9493b0d20df8c0aef709c6dd8fd785593
fancyzhong <fancyzhong@tencent.com>
2021-08-20 09:10:26 +0800

    feat:pub

---
+++ precache-manifest.7404d443f6755b28a0f3fa1f10175352.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca064e1000d18941dcb06fc483e5c552",
+    "url": "https://tnfe.github.io/tntweb-admin/editor.worker.js"
+  },
+  {
+    "revision": "d7dd4698be6e26e5ea7b1293ec7aec5c",
+    "url": "https://tnfe.github.io/tntweb-admin/index.html"
+  },
+  {
+    "revision": "f2ef3af0968577206259fd5755b07d7c",
+    "url": "https://tnfe.github.io/tntweb-admin/json.worker.js"
+  },
+  {
+    "revision": "332d7488f7c2b19a7e78",
+    "url": "https://tnfe.github.io/tntweb-admin/static/css/0.3ee9a256.chunk.css"
+  },
+  {
+    "revision": "9dd7f6342ca23b6b5c03",
+    "url": "https://tnfe.github.io/tntweb-admin/static/css/5.1ebddd3b.chunk.css"
+  },
+  {
+    "revision": "960ab1713ba7bb2eca6f",
+    "url": "https://tnfe.github.io/tntweb-admin/static/css/6.ec20a7bc.chunk.css"
+  },
+  {
+    "revision": "20b0ae1dced90f1edf2d",
+    "url": "https://tnfe.github.io/tntweb-admin/static/css/main.f8179887.chunk.css"
+  },
+  {
+    "revision": "332d7488f7c2b19a7e78",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/0.d09f2b51.chunk.js"
+  },
+  {
+    "revision": "5bac5b5f89028fdec9f0",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/1.246c03e3.chunk.js"
+  },
+  {
+    "revision": "b80415c3d68c44d8894f",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/10.fc89d7ab.chunk.js"
+  },
+  {
+    "revision": "36b7e1895e1363d54986",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/11.91286f90.chunk.js"
+  },
+  {
+    "revision": "901b3dab17a5e19cb7b6",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/12.06ffa236.chunk.js"
+  },
+  {
+    "revision": "f2d9e7ec30a4804ef0bd",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/13.ea021599.chunk.js"
+  },
+  {
+    "revision": "87f69202486d4b6c1a3a",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/4.6c5b5e90.chunk.js"
+  },
+  {
+    "revision": "a10491a56f18523652cf0dbbcd945243",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/4.6c5b5e90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd7f6342ca23b6b5c03",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/5.0a4cbbac.chunk.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/5.0a4cbbac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "960ab1713ba7bb2eca6f",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/6.fec59d84.chunk.js"
+  },
+  {
+    "revision": "29c4bbcf91c3eaffa6ed",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/7.a7c5b270.chunk.js"
+  },
+  {
+    "revision": "bff962039ffb21d940b4",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/8.9533956e.chunk.js"
+  },
+  {
+    "revision": "9e64dc1829a1fb736210",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/9.091dda64.chunk.js"
+  },
+  {
+    "revision": "20b0ae1dced90f1edf2d",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/main.4bf3c499.chunk.js"
+  },
+  {
+    "revision": "b7800556d7e4a61a5564",
+    "url": "https://tnfe.github.io/tntweb-admin/static/js/runtime-main.46983d26.js"
+  },
+  {
+    "revision": "a1cef3d530e1adb0f52b2f62994a2aca",
+    "url": "https://tnfe.github.io/tntweb-admin/static/media/codicon.ttf"
+  },
+  {
+    "revision": "7ac2c24d999679bc7683ee710671df49",
+    "url": "https://tnfe.github.io/tntweb-admin/ts.worker.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://tnfe.github.io/tntweb-admin/ts.worker.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://tnfe.github.io/tntweb-admin/precache-manifest.d00af5eeb54134f7efdfbcc8480b502e.js"
+  "https://tnfe.github.io/tntweb-admin/precache-manifest.7404d443f6755b28a0f3fa1f10175352.js"
 );
 
 self.addEventListener('message', (event) => {
